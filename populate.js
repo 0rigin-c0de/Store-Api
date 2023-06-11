52163deafefb7e99d2efe461fe7ac6f9834fa2c6
0rigin-c0de <shunnu807@gmail.com>
2023-06-11 08:25:13 +0530

    initial-commit

---
+++ populate.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+
+const connectDB = require("./db/connect");
+const Product = require("./models/products");
+
+const jsonProducts = require("./products.json");
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    await Product.deleteMany();
+    await Product.create(jsonProducts);
+    console.log("Successful");
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
