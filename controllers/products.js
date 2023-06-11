45cae564701e2b97cf7941bc8b72421df4418160
0rigin-c0de <shunnu807@gmail.com>
2023-06-11 08:25:22 +0530

    initial-commit

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,13 @@
+const Product = require("../models/products");
+
 const getAllProductsStatic = async (req, res) => {
-  res.status(200).json({ msg: "products testing route" });
+  const products = await Product.find({ featured: false });
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "products route" });
+  const products = await Product.find(req.query);
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = {
